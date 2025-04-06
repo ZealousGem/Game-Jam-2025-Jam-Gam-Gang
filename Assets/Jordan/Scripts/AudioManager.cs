2543c0f369c21a5af24d0a15c200516a3d29c9c0
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2025-04-06 19:25:41 +0200

    made screen resolution and AudioManager

---
+++ Assets/Jordan/Scripts/AudioManager.cs
@@ -0,0 +1,82 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Sound
+{
+    AudioSource source;
+    public string nameClip;
+    public AudioClip clip;
+
+    [Range (0f, 1f)]
+    public float volume;
+    [Range (0f, 3f)]
+    public float pitch;
+
+    public bool loop = false;
+    public bool isPlaying = false;
+
+    public void setSource(AudioSource sourceClip)
+    {
+        this.source = sourceClip;
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.playOnAwake = isPlaying;
+        source.loop = loop;
+    }
+
+    public void Play()
+    {
+        source.Play();
+    }
+}
+public class AudioManager : MonoBehaviour
+{
+   public static AudioManager Instance;
+
+
+    [SerializeField]
+    Sound[] sounds;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }  
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject play = new GameObject("Sound : " + i + " : " + sounds[i].nameClip);
+            play.transform.SetParent(this.transform);
+            sounds[i].setSource(play.AddComponent<AudioSource>());
+        } 
+    }
+
+    public void PlaySound(string name)
+    {
+        for (int i = 0; i< sounds.Length; i++)
+        {
+            if (sounds[i].nameClip == name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
---
+++ Assets/Jordan/Scripts/ScreenResolution.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScreenResolution : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Resolution[] sizes;
+    // public Dropdown dropdown;
+    public TMP_Dropdown dropdown;
+    void Start()
+    {
+        sizes = Screen.resolutions;
+
+        dropdown.ClearOptions();
+        List<string> option = new List<string>();
+        int sizesIndex = 0;
+
+        for (int i = 0; i < sizes.Length; i++)
+        {
+            string choice = sizes[i].width + "x" + sizes[i].height;
+            option.Add(choice);
+
+            if (sizes[i].width == Screen.currentResolution.width && sizes[i].height == Screen.currentResolution.height)
+            {
+                sizesIndex = i;
+            }
+        }
+
+        dropdown.AddOptions(option);
+        dropdown.value = sizesIndex;
+        dropdown.RefreshShownValue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FullScreen(bool toggle)
+    {
+        Screen.fullScreen = toggle;
+    }
+
+    public void setSize(int resIndex)
+    {
+        Resolution resolution = sizes[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
