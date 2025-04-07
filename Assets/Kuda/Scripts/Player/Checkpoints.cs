e145f28b9d042a4a3e8782f7139b6f9de886958e
GlitzySugar <106977034+GlitzySugar@users.noreply.github.com>
2025-04-07 12:12:30 +0200

    Update Checkpoints.cs
    Updated script

--- Assets/Kuda/Scripts/Player/Checkpoints.cs
+++ Assets/Kuda/Scripts/Player/Checkpoints.cs
@@ -4,17 +4,17 @@ public class Checkpoints : MonoBehaviour
 {
     GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        player = GameObject.FindWithTag("Player");
-    }
 
     private void Awake()
+    {
+        Respawn();
+    }
+
+    public void Respawn()
     {
         player = GameObject.FindWithTag("Player");
         player.transform.position = this.transform.position;
     }
-
     // Update is called once per frame
 
 }
