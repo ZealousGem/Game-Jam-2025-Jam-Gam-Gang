681be91dfea2bd72250d0f778222288234e1169a
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2025-04-07 16:46:06 +0200

    Added Boxes to Level 2

--- Assets/Jordan/Scripts/PlayerController.cs
+++ Assets/Jordan/Scripts/PlayerController.cs
@@ -83,7 +83,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("PickedUp"))
         {
             isGrounded = true;
 
