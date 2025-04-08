fbfd657201db31493a994c35160f24ca89f30e59
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2025-04-08 21:50:43 +0200

    fixed SafeUI

--- Assets/Kaellum/Scripts/Interactables/Safe.cs
+++ Assets/Kaellum/Scripts/Interactables/Safe.cs
@@ -12,11 +12,13 @@ public class Safe : Interactable
     [SerializeField] private GameObject Quit;
     [SerializeField] private GameObject numChange;
     [SerializeField] private GameObject SafePanel;
+    [SerializeField] private GameObject UI;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        UI.SetActive(false);
         Quit.SetActive(false);
         SafePanel.SetActive(false);
         numChange.SetActive(false);
@@ -46,6 +48,27 @@ public class Safe : Interactable
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UI.SetActive(true);
+
+        }
+
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UI.SetActive(false);
+
+        }
     }
 
 }
